39b8164f5962866e0d750a1342d202a518d827aa
David Ibia <david.ibia@boxmarshall.com>
2022-12-15 10:44:19 +0100

    feat: instantiate pool

---
+++ src/helpers/sequelize-pool/index.ts
@@ -0,0 +1,23 @@
+import { Sequelize, Options, Dialect } from "sequelize";
+import { sqlConfig } from "@/config";
+
+const connectionPoolOptions: Options = {
+    host: sqlConfig.host,
+    dialect: (sqlConfig.connection as Dialect) ?? "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 60000,
+        idle: 20000,
+    },
+    logging: false,
+};
+
+const connectionPool = new Sequelize(
+    sqlConfig.database,
+    sqlConfig.user,
+    sqlConfig.pass,
+    connectionPoolOptions
+);
+
+export default connectionPool;
