7447b8cfb4fdcac52126d0090f1c26e75bfd54df
David Ibia <david.ibia@boxmarshall.com>
2023-08-14 16:02:26 +0100

    refactor(index.ts): reorganize code for better readability and maintainability feat(index.ts): add logging statements for connecting to SQL Database and starting the server feat(index.ts): log the mode (production or development) the server is running in feat(index.ts): log the port the server is started on feat(index.ts): add routes to the app

--- src/index.ts
+++ src/index.ts
@@ -5,35 +5,23 @@ import pool from "@/helpers/sequelize-pool";
 import routes from "@/routes";
 import { port, isProduction, allowedDomains } from "@/config";
 
-async function startServer() {
-    try {
-        // Connect to SQL Database
+logger.log("Connecting to SQL Database");
+await pool.authenticate();
+logger.info("Connected to SQL Database");
 
-        logger.log("Connecting to SQL Database");
-        await pool.authenticate();
-        logger.info("Connected to SQL Database");
+// Start Server
+logger.log("Starting Server");
 
-        // Start Server
-        logger.log("Starting Server");
+logger.info(`Running in ${isProduction ? "production" : "development"} mode`);
 
-        logger.info(
-            `Running in ${isProduction ? "production" : "development"} mode`
-        );
+const options: CreateServerOptions = {
+    port,
+    production: isProduction,
+    whitelistedDomains: allowedDomains,
+};
 
-        const options: CreateServerOptions = {
-            port,
-            production: isProduction,
-            whitelistedDomains: allowedDomains,
-        };
+const app = await createServer(options);
 
-        const app = await createServer(options);
+logger.info(`Server started on port ${port}`);
 
-        logger.info(`Server started on port ${port}`);
-
-        app.use(routes);
-    } catch (err) {
-        logger.error(err);
-    }
-}
-
-startServer();
+app.use(routes);
